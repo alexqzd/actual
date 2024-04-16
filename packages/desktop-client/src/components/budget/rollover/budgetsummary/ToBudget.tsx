cb50460fb96814a1a2864b13bdd2cb64f5331745
alexqzd <alexqm@icloud.com>
2024-04-15 19:04:28 -0600

    Fix income forecast logic for status paid in future date

--- packages/desktop-client/src/components/budget/rollover/budgetsummary/ToBudget.tsx
+++ packages/desktop-client/src/components/budget/rollover/budgetsummary/ToBudget.tsx
@@ -47,8 +47,8 @@ export function getDateWithSkippedWeekend(
   return date;
 }
 
-function getScheduleOcurrencesUpToMonth({ config, month }) {
-
+function getScheduleOcurrencesUpToMonth({ s, month }) {
+  const config = s._date;
     // if the frequency is undefined, we assume it's a one-time schedule
     if (!config.frequency) {
       // if one-time schedule, we return the date it's supposed to happen if it has happened before or on the given month
@@ -62,7 +62,7 @@ function getScheduleOcurrencesUpToMonth({ config, month }) {
     const schedule = new RSchedule({ rrules: rules });
 
 
-    var count = 1;
+    var count = 366;
 
     if (config.frequency === 'daily') {
       count = 31;
@@ -75,10 +75,8 @@ function getScheduleOcurrencesUpToMonth({ config, month }) {
     const monthIndex = Number(yearMonth.slice(5, 7)) - 1; // month is 0-indexed
     const firstDayMonth = new Date(year, monthIndex, 1);
 
-
     return schedule
-    // start today, end the last day of the given month
-      .occurrences({ start: currentDate(), end: d.endOfMonth(firstDayMonth), take: 365 })
+      .occurrences({ start: s.next_date, end: d.endOfMonth(firstDayMonth), take: count })
       .toArray()
       .map(date =>
         config.skipWeekend
@@ -156,13 +154,22 @@ export function ToBudget({
   const schedulesThisMonth = useMemo(() => {
     const schedulesThisMonth = [];
     schedules.forEach(s => {
-      var occurrences = getScheduleOcurrencesUpToMonth({ config: s._date, month: month });
+      var occurrences = getScheduleOcurrencesUpToMonth({ s: s, month: month });
+      console.log(s.name, "occurrences", occurrences, "in month", month, "status", scheduleData.statuses.get(s.id));
       const alreadyPaid = scheduleData.statuses.get(s.id) === 'paid';
       // if already paid, we don't want to count it if the month we are processing is the current month
       // because the schedule is only shown as paid in the day of the schedule
       if (alreadyPaid) {
-        // drop first occurrence because it's the one that has already been paid
-        occurrences = occurrences.slice(1);
+        // if the schedule is paid, we want to remove the first occurrence
+        // because it's the one that has already happened
+        // only if today's date is greater or equal to the next date
+        const today = currentDate();
+        const nextDate = new Date(s.next_date); // Convert s.next_date to a Date object
+        console.log(s.name, "is already paid, next date", nextDate, "today", today);
+        if (today >= nextDate) {
+          console.log(s.name, "is already paid, removing first occurrence");
+          occurrences = occurrences.slice(1);
+        }
       }
       if (!alreadyPaid || monthFromDate(s.next_date) !== month) {
         const timesThisMonth = occurrences.length;
